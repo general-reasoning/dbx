e90df230338a75be85a7bffdd31eaff8178eb321
Dmitry Karpeyev <dk@faction.co>
2026-06-29 21:49:18 +0000

    datablocks: fix copy_topic_dir ensure only for list-mode TOPICS

--- dbx/datablocks.py
+++ dbx/datablocks.py
@@ -1434,7 +1434,7 @@ class Datablock:
 
         def copy_topic_dir(topic):
             """Copy the entire .dirpath(topic) directory."""
-            dst_path = self.dirpath(topic, ensure=True)
+            dst_path = self.dirpath(topic, ensure=self._topicfiles is None)
             if topicpaths is not None:
                 _src_path = topicpaths[topic]
             else:
